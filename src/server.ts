9b5960bb0c104333e04524f7ca6f807cbf23939c
Felipe <lipe1090@icloud.com>
2025-04-04 19:28:00 -0300

    fix: AppEngine

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,3 @@
-import { CommonEngine } from "@angular/ssr/node";
-import { render } from "@netlify/angular-runtime/common-engine";
-
-const commonEngine = new CommonEngine();
-
-export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-  // Example API endpoints can be defined here.
-  // Uncomment and define endpoints as necessary.
-  // const pathname = new URL(request.url).pathname;
-  // if (pathname === '/api/hello') {
-  //   return Response.json({ message: 'Hello from the API' });
-  // }
-
-  return await render(commonEngine);
-}
-
-// If you want to use "AppEngine" instead - your server.ts file should contain following:
-
 import { AngularAppEngine, createRequestHandler } from "@angular/ssr";
 import { getContext } from "@netlify/angular-runtime/context";
 
