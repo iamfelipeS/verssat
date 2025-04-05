5f73850fd8f9b2b5a4d6d5ce189bbd963c3edefd
Felipe <lipe1090@icloud.com>
2025-04-05 14:12:44 -0300

    chore: reset projeto com Angular 18 e SSR

--- src/app/models/leagues.model.ts
+++
@@ -1,8 +0,0 @@
-import { Organizer } from "./organizer.model";
-
-export interface Leagues {
-    name: string;
-    organizer: Organizer[];
-    private: boolean;
-    img?:string;
-}
--- src/app/models/organizer.model.ts
+++
@@ -1,7 +0,0 @@
-
-
-export interface Organizer{
-    name: string;
-    phone?:string;
-    email?: string;
-}
